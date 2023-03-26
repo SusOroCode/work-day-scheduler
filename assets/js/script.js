3cc89f60393d8b076953dd3578046d68ae2e2d0a
Tanuki <susann.orosco33@gmail.com>
2023-03-25 19:02:28 -0700

    finished the color change, fixed a spelling error (I asked the AskBot in Slack)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,8 @@
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 // Resource: http://learn.jquery.com/using-jquery-core/document-ready/
 // Resource: https://stackoverflow.com/questions/71781877/get-data-value-of-parent-of-clicked-child
 // Respurce: https://stackoverflow.com/questions/70470393/how-select-a-sibling-of-parent-element-of-this-in-jquery
 
-//var currentDayEl = moment().format('dddd MMM Do YYYY');
-//$('#currentDay').html(currentDayEl);
-//console.log("Does it diplay?");
 
 // TODO: Add code to display the current date in the header of the page.
 
@@ -35,7 +27,7 @@ $(document).ready(function () {
 
 
   //Track hours and change colors past present and future
-
+  // Asked AskBot for help, i had a spelling error addclass instead of addClass
   trackHour();
 
   function trackHour() {
@@ -47,7 +39,7 @@ $(document).ready(function () {
       console.log(currentHourEl);
 
       if (currentHourEl < hourEl) {
-        $(this).removeclass('present');
+        $(this).removeClass('present');
         $(this).removeClass('future');
         $(this).addClass('past');
       } else if (currentHourEl === hourEl) {
@@ -66,6 +58,7 @@ $(document).ready(function () {
   }
 });
 
+//Local Storage
 
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
@@ -81,32 +74,8 @@ $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  // To do: research about past, present and future class and how to ocmpare the id of current hour,
-  // time-block, how to add or remove?
 
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-  // https://www.geeksforgeeks.org/moment-js-moment-hour-method/
-
-
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
 
 
