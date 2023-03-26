946666323f1f41d4b448a73a6977d5bec518a917
Tanuki <susann.orosco33@gmail.com>
2023-03-25 18:22:17 -0700

    added local storage to keep items after page is refreshed, worked on color change of time slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,95 +18,64 @@
 $(document).ready(function () {
 
   //Display day and time in header
-
   var currentTimeEl = dayjs();
   $('#currentDay').text(currentTimeEl.format('dddd, MMMM D YYYY, h:mm:ss a'));
 
+  //Button to save data in local storage
 
- 
-  
+  $('.saveBtn').on('click', function () {
+    var textEl = $(this).siblings(".description").val();
+    var timeEl = $(this).parent().attr("id");
 
-  trackHour(function()) {
+    localStorage.setItem(timeEl, textEl);  // Save in local storage
 
-  //var hour = currentDay.hours();
-  var hour = dayjs().format('H');
+    console.log('did it save?'); // Yes it saved!
 
-  $('.time-block').each(function () {
-    var currentHourEl = parseInt($(this).attr('id').split('hour')[1]);
-
-    if (currentHourEl < hour) {
-      $(this).addclass('past');
-      $(this).removeClass('future');
-      $(this).addClass('present');
-    } else if (currentHourEl === hour) {
-      $(this).removeClass('past');
-      $(this).addClass('future');
-      $(this).removeClass('present');
-    } else {
-      $(this).removeClass('present');
-      $(this).removeClass('past');
-      $(this).addClass('future');
-    }
-  
   });
+
+
+  //Track hours and change colors past present and future
+
+  trackHour();
+
+  function trackHour() {
+
+    var hourEl = dayjs().format('H')
+
+    $('.time-block').each(function () {
+      var currentHourEl = parseInt($(this).attr('id').split('-')[1]);
+      console.log(currentHourEl);
+
+      if (currentHourEl < hourEl) {
+        $(this).removeclass('present');
+        $(this).removeClass('future');
+        $(this).addClass('past');
+      } else if (currentHourEl === hourEl) {
+        $(this).removeClass('past');
+        $(this).removeClass('future');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+
+
+
+    });
   }
 });
 
-$('.saveBtn').on('click', function () {
-  var textEl = $(this).siblings(".description").val();
-  var timeEl = $(this).parent().attr("id");
 
-  localStorage.setItem(timeEl, textEl);  // Save in local storage
-
-  console.log('did it save?'); // Yes it saved!
-});
-
-
-$('hour-9 .description').val(localStorage.getItem('hour-9'));
-$('hour-10 .description').val(localStorage.getItem('hour-10'));
-$('hour-11 .description').val(localStorage.getItem('hour-11'));
-$('hour-12 .description').val(localStorage.getItem('hour-12'));
-$('hour-13 .description').val(localStorage.getItem('hour-13'));
-$('hour-14 .description').val(localStorage.getItem('hour-14'));
-$('hour-15 .description').val(localStorage.getItem('hour-15'));
-$('hour-16 .description').val(localStorage.getItem('hour-16'));
-$('hour-17 .description').val(localStorage.getItem('hour-17'));
-
-
-function scheduleDay() {
-  $('.hour').each(function () {
-    var currentHourEl = $(this).text();
-    var currDayTime = localStorage.getItem(currentHourEl);
-
-    if (currDayTime !== null) {
-      $(this).siblings('.description').val(currDayTime);
-    }
-  });
-}
-
-scheduleDay();
-
-
-
-  //var timeBlockEl = $('.time-block');
-  //console.log(timeBlockEl);
-
-  //
-
-
-  //console.log(this);
-
-
-
-  //var currentDayEl = document.querySelector('#currentDay'); // selects current day(time)
-
-  //function displayTime() {
-  // currentDayEl.textContent = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a'); // function to display day, month, year and time
-  //}
-
-  //displayTime(); // call function
-  //setInterval(displayTime, 1000);  //calls function at specified intervals in milliseconds
-  //var saveBtnEl = $('.saveBtn');
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
 
@@ -123,14 +92,6 @@ scheduleDay();
   // time-block, how to add or remove?
 
 
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
@@ -141,8 +102,12 @@ scheduleDay();
 
 
 
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
 
-  
 
 
 
